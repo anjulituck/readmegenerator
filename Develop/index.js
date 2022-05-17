6e1edf05a3343ddecbd059db5e860e58bf9d2567
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-05-17 18:02:42 -0500

    added README basics to function

---
+++ Develop/index.js
@@ -0,0 +1,56 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+ return `
+ # <Your-Project-Title>
+
+## Description
+
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+
+## Usage
+
+
+## Credits
+
+
+## License
+
+
+## Badges
+
+
+## Features
+
+
+## How to Contribute
+
+
+## Tests
+
+ 
+ `
+
+
+
+
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
